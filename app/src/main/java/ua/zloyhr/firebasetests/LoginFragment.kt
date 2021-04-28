a441d6d9eac19397a1fa90778f6a04b78718b4f9
ZloyHR <dobrovolskyivan2002@gmail.com>
2021-04-28 14:10:43 +0300

    Moved save logic to LoginFragment.kt Connected button to savePerson

--- app/src/main/java/ua/zloyhr/firebasetests/LoginFragment.kt
+++ app/src/main/java/ua/zloyhr/firebasetests/LoginFragment.kt
@@ -1,8 +1,50 @@
 package ua.zloyhr.firebasetests
 
+import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-class LoginFragment : Fragment(R.layout.fragment_login) {
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import ua.zloyhr.firebasetests.databinding.FragmentLoginBinding
 
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    private val persons = FirebaseFirestore.getInstance().collection("persons")
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentLoginBinding.bind(view)
+
+        binding.apply {
+            btnSendButton.setOnClickListener {
+                val person: Person
+                try{
+                    person = Person(
+                        tfFirst.editText?.text.toString(),
+                        tfLast.editText?.text.toString(),
+                        tfAge.editText?.text.toString().toInt()
+                    )
+                    savePerson(person)
+                }catch (it: Exception){
+                    Toast.makeText(requireContext(), "Error while creating person: ${it.message}", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+    }
+
+    private fun savePerson(person: Person) {
+        persons.add(person)
+            .addOnSuccessListener {
+                Toast.makeText(requireContext(), "Added user to db with id: ${it.id}", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(requireContext(), "Error while adding to db: ${it.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+    }
 }
 
