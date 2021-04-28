b8bdaccce36f8c5e06ed93bd06634022fa936990
ZloyHR <dobrovolskyivan2002@gmail.com>
2021-04-28 13:29:44 +0300

    Added Person.kt data class Added savePerson in MainActivity.kt

--- app/src/main/java/ua/zloyhr/firebasetests/MainActivity.kt
+++ app/src/main/java/ua/zloyhr/firebasetests/MainActivity.kt
@@ -2,10 +2,32 @@ package ua.zloyhr.firebasetests
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private val persons = FirebaseFirestore.getInstance().collection("persons")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun savePerson(person: Person) {
+        persons.add(person)
+            .addOnSuccessListener {
+                Toast.makeText(this,"Added user to db with id: ${it.id}",Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this,"Error while adding to db: ${it.message}",Toast.LENGTH_LONG).show()
+            }
+    }
+
+
 }
---
+++ app/src/main/java/ua/zloyhr/firebasetests/Person.kt
@@ -0,0 +1,8 @@
+package ua.zloyhr.firebasetests
+
+data class Person(
+    var firstName: String,
+    var secondName: String,
+    var age: Int,
+) {
+}
